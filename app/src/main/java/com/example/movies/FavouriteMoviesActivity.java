0eaf93c8eb18c495a4d6e1d15a72aad6069001fe
ri-gusev <gusev.roman1@mail.ru>
2025-04-30 04:52:31 +0300

    Create favouriteMoviesActivity

---
+++ app/src/main/java/com/example/movies/FavouriteMoviesActivity.java
@@ -0,0 +1,58 @@
+package com.example.movies;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class FavouriteMoviesActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private MovieAdapter movieAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_favourite_movies);
+        initViews();
+
+        FavouriteMoviesViewModel favouriteMoviesViewModel = new ViewModelProvider(this).get(
+                FavouriteMoviesViewModel.class);
+
+        favouriteMoviesViewModel.getAllMovies().observe(this, new Observer<List<Movie>>() {
+            @Override
+            public void onChanged(List<Movie> movies) {
+                movieAdapter.setMovieList(movies);
+            }
+        });
+
+        movieAdapter.setOnClickListener(new MovieAdapter.onClickListener() {
+            @Override
+            public void onClick(Movie movie) {
+                startActivity(MovieDetailActivity.newIntent(
+                        FavouriteMoviesActivity.this,
+                        movie));
+            }
+        });
+    }
+
+    public static Intent newIntent(Context context){
+        return new Intent(context, FavouriteMoviesActivity.class);
+    }
+
+    public void initViews() {
+        recyclerView = findViewById(R.id.RecyclerViewFavouriteMovies);
+        movieAdapter = new MovieAdapter();
+        recyclerView.setAdapter(movieAdapter);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+    }
+}
---
+++ app/src/main/java/com/example/movies/FavouriteMoviesViewModel.java
@@ -0,0 +1,25 @@
+package com.example.movies;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.List;
+
+public class FavouriteMoviesViewModel extends AndroidViewModel {
+
+    private final MovieDao movieDao;
+
+    public FavouriteMoviesViewModel(@NonNull Application application) {
+        super(application);
+        movieDao = MovieDatabase.getInstance(application).movieDao();
+    }
+
+    public LiveData<List<Movie>> getAllMovies(){
+        return movieDao.getAllFavouritesMovies();
+    }
+
+}
